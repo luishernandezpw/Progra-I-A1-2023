244ef64db43ae24d7a8ec9ca59c5be90f6867103
Luis Hernandez <sarco_luis@hotmail.com>
2023-11-22 10:09:55 -0600

    se comenzo desde cero el formulario avanzado de inscripcion de materias

--- ejercicios/inscripcion.cs
+++ ejercicios/inscripcion.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,17 +13,10 @@ namespace ejercicios
 {
     public partial class inscripcion : Form
     {
+        private int _idInscripcion = 0;
         public inscripcion()
         {
             InitializeComponent();
-        }
-
-        private void inscripcionBindingNavigatorSaveItem_Click(object sender, EventArgs e)
-        {
-            this.Validate();
-            this.inscripcionBindingSource.EndEdit();
-            this.tableAdapterManager.UpdateAll(this.db_academicoDataSet);
-
         }
 
         private void inscripcion_Load(object sender, EventArgs e)
@@ -31,11 +25,127 @@ namespace ejercicios
             this.dtDetalleInscripcionMateria.Fill(this.db_academicoDataSet.dInscripcionMateria);
             // TODO: esta línea de código carga datos en la tabla 'db_academicoDataSet.alumnos' Puede moverla o quitarla según sea necesario.
             this.alumnosTableAdapter.Fill(this.db_academicoDataSet.alumnos);
-            // TODO: esta línea de código carga datos en la tabla 'db_academicoDataSet.matriculaAlumnosPeriodos' Puede moverla o quitarla según sea necesario.
-            this.dtMatriculaAlumnosPeriodos.Fill(this.db_academicoDataSet.matriculaAlumnosPeriodos);
-            // TODO: esta línea de código carga datos en la tabla 'db_academicoDataSet.inscripcion' Puede moverla o quitarla según sea necesario.
-            this.inscripcionTableAdapter.Fill(this.db_academicoDataSet.inscripcion);
+            // TODO: esta línea de código carga datos en la tabla 'db_academicoDataSet.inscripcionMatriculaPerido' Puede moverla o quitarla según sea necesario.
+            this.inscripcionMatriculaPeridoTableAdapter.Fill(this.db_academicoDataSet.inscripcionMatriculaPerido);
 
+            nRegistroInscripcion();
+        }
+        private void nRegistroInscripcion()
+        {
+            lblnRegistroInscripcion.Text = (inscripcionMatriculaPeridoBindingSource.Position + 1) + " de " + inscripcionMatriculaPeridoBindingSource.Count;
+        }
+        private void estadoControles(Boolean estado)
+        {
+            fechaDateTimePicker.Enabled = estado;
+            idAlumnoComboBox.Enabled = estado;
+
+            grbNavegacionInscripcion.Enabled = !estado;
+            btnEliminarInscripcion.Enabled = !estado;
+
+            btnAgregarMateriaInscripcion.Enabled = estado;
+            btnEliminarMateriaInscripcion.Enabled = estado;
+        }
+        private void btnSiguienteInscripcion_Click(object sender, EventArgs e)
+        {
+            inscripcionMatriculaPeridoBindingSource.MoveNext();
+            nRegistroInscripcion();
+        }
+
+        private void btnUltimoInscripcion_Click(object sender, EventArgs e)
+        {
+            inscripcionMatriculaPeridoBindingSource.MoveLast();
+            nRegistroInscripcion();
+        }
+
+        private void btnAnteriorInscripcion_Click(object sender, EventArgs e)
+        {
+            inscripcionMatriculaPeridoBindingSource.MovePrevious();
+            nRegistroInscripcion();
+        }
+
+        private void btnPrimeroInscripcion_Click(object sender, EventArgs e)
+        {
+            inscripcionMatriculaPeridoBindingSource.MoveFirst();
+            nRegistroInscripcion();
+        }
+
+        private void btnNuevoInscripcion_Click(object sender, EventArgs e)
+        {
+            if (btnNuevoInscripcion.Text == "Nuevo") {
+                btnNuevoInscripcion.Text = "Guardar";
+                btnModificarInscripcion.Text = "Cancelar";
+                estadoControles(true);
+                inscripcionMatriculaPeridoBindingSource.AddNew();
+                idAlumnoComboBox.SelectedValue = 1;
+
+            }else{//Guardar
+                try
+                {
+                    this.Validate();
+                    inscripcionMatriculaPeridoBindingSource.EndEdit();
+
+                    inscripcionMatriculaPeridoTableAdapter.Connection.Open();
+                    SqlCommand miCommand = new SqlCommand();
+                    miCommand.Connection = inscripcionMatriculaPeridoTableAdapter.Connection;
+                    miCommand.CommandText = "SELECT ident_current('inscripcion') + 1 AS idInscripcion";
+                    _idInscripcion = int.Parse(miCommand.ExecuteScalar().ToString());
+                    inscripcionMatriculaPeridoTableAdapter.Connection.Close();
+
+                    int nfilas = dInscripcionMateriaDataGridView.Rows.Count;
+                    string[,] valores = new string[nfilas,1];
+                    for(int i=0; i<nfilas-1; i++){
+                        valores[i, 0] = dInscripcionMateriaDataGridView.Rows[i].Cells["idMateria"].Value.ToString();
+                    }
+                    tableAdapterManager.UpdateAll(this.db_academicoDataSet);
+                    for (int i = 0; i < nfilas - 1; i++) {
+                        //dtDetalleInscripcionMateria valores[i, 0]
+                    }
+
+
+                    btnNuevoInscripcion.Text = "Nuevo";
+                    btnModificarInscripcion.Text = "Modificar";
+                    estadoControles(false);
+                }catch(Exception error)
+                {
+                    MessageBox.Show("Error: " + error.Message);
+                }
+            }
+        }
+
+        private void btnModificarInscripcion_Click(object sender, EventArgs e)
+        {
+            if (btnModificarInscripcion.Text == "Modificar"){
+                btnNuevoInscripcion.Text = "Guardar";
+                btnModificarInscripcion.Text = "Cancelar";
+                estadoControles(true);
+
+            }else{//Cancelar
+                dInscripcionMateriaBindingSource.CancelEdit();
+                inscripcionMatriculaPeridoBindingSource.CancelEdit();
+
+                btnNuevoInscripcion.Text = "Nuevo";
+                btnModificarInscripcion.Text = "Modificar";
+                estadoControles(false);
+            }
+        }
+
+        private void btnAgregarMateriaInscripcion_Click(object sender, EventArgs e)
+        {
+            materias objMateria = new materias();
+            objMateria.ShowDialog();
+            if( objMateria._idMateria>0) {
+                dInscripcionMateriaBindingSource.AddNew();
+
+                dInscripcionMateriaDataGridView.CurrentRow.Cells["idMateria"].Value = objMateria._idMateria;
+                dInscripcionMateriaDataGridView.CurrentRow.Cells["codigo"].Value = objMateria._codigo;
+                dInscripcionMateriaDataGridView.CurrentRow.Cells["materia"].Value = objMateria._materia;
+                dInscripcionMateriaDataGridView.CurrentRow.Cells["uv"].Value = objMateria._uv;
+            }
+        }
+
+        private void btnEliminarMateriaInscripcion_Click(object sender, EventArgs e)
+        {
+            dInscripcionMateriaDataGridView.Rows.Remove(dInscripcionMateriaDataGridView.CurrentRow);
         }
     }
 }
